edfa77e1728e1133482fc56b056ae4d55dcf3a54
Nicolas Freitas Pereira <nicolas.freitas27@hotmail.com>
2025-04-22 15:58:34 -0300

    Rotas de audienceQuestions criadas e funcionando

--- backend/routes/audienceQuestions.js
+++ backend/routes/audienceQuestions.js
@@ -1,10 +1,14 @@
 // routes/audienceQuestions.js
 const express = require('express');
 const router = express.Router();
+const audienceQuestionController = require('../controllers/audienceQuestionController');
 
 // Temporário: controlador ainda não implementado
 router.get('/', (req, res) => {
   res.status(200).json({ message: 'Rota de perguntas da audiência funcionando' });
 });
+
+router.post('/create', audienceQuestionController.createAudienceQuestion);
+router.put('/like/:id', audienceQuestionController.likeAudienceQuestion);
 
 module.exports = router;
